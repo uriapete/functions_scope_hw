83c484617dd6a6fa8117dc6fc053771a235f625f
uriapete <uriarteangelo@gmail.com>
2023-02-13 14:09:24 -0800

    C++ - calcCube

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+
+//calculate cube
+int calculateCube(int num) {return pow(num, 3);}
 
 int main() {
-    
+    std::cout << "Calculate Cube: " << std::to_string(calculateCube(5)) << "\n\n";
 }
