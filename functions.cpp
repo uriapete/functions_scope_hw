1fd06c68813c31b52757a15499305a1af1a87028
uriapete <uriarteangelo@gmail.com>
2023-02-13 14:53:29 -0800

    C++ - GetTwoLengths

--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <array>
+#include <vector>
 #include <cmath>
 
 //calculate cube
@@ -19,12 +21,35 @@ bool isAVowel(char letter) {
     
 }
 
+//get2Lengths
+
+std::array<int,2> getTwoLengths(std::string word1, std::string word2) {
+    std::array<int,2> twoLengths = {word1.length(), word2.length()};
+    return twoLengths;
+}
+
 int main() {
     // CalcCube
     std::cout << "Calculate Cube: " << std::to_string(calculateCube(5)) << "\n\n";
+
     // IsVowel
     std::cout << "Is a Vowel: \n" << "'A' is a Vowel: " << std::to_string(isAVowel('A')) << "\n";
     std::cout << "Is a Vowel: \n" << "'B' is a Vowel: " << std::to_string(isAVowel('B')) << "\n";
     std::cout << "Is a Vowel: \n" << "'a' is a Vowel: " << std::to_string(isAVowel('a')) << "\n";
     std::cout << "Is a Vowel: \n" << "'b' is a Vowel: " << std::to_string(isAVowel('b')) << "\n\n";
+
+    // get2Lengths
+    std::array<int,2> get2LTest = getTwoLengths("Hank", "Hippopopalous");
+    std::cout << "Get two lengths: (\"Hank\", \"Hippopopalous\"): {";
+    for (int i = 0; i < 2; i++)
+    {
+        std::cout << get2LTest[i];
+        if (i < 1)
+        {
+            std::cout << ", ";
+        }
+        
+    }
+    std::cout << "}\n\n";
+    
 }
